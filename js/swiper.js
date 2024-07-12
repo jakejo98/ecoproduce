3fb9a4f4e5a32c167299ef291c8d098c5ea429f2
jakejo <jakejo@kakao.com>
2024-07-12 17:45:51 +0900

    [REFACTOR] 이벤트 배너 수정 진행중

--- js/swiper.js
+++ js/swiper.js
@@ -1,10 +1,15 @@
 export function swiper() {
+  mainBannerSwiperCon();
+  eventBannerSwiperCon();
+}
+
+function mainBannerSwiperCon() {
   const cont = $('.section_main_banner'); // 마우스 이벤트를 받을 요소 선택
   const btnEffect = 'active'
   const prevBtn = $('.swiper-button-prev')
   const nextBtn = $('.swiper-button-next')
   // 스와이퍼 기본속성
-  var swiper = new Swiper('.main_banner', {
+  var mainBannerSwiper = new Swiper('.main_banner', {
     slidesPerView : 'auto', 
     loopAdditionalSlides: 1, 
     centeredSlides: true,
@@ -24,8 +29,8 @@ export function swiper() {
     },
     a11y: { 
       enabled: true,
-      prevSlideMessage: '이전 배너를 이동',
-      nextSlideMessage: '다음 배너를 이동',   
+      prevSlideMessage: '이전 배너로 이동',
+      nextSlideMessage: '다음 배너로 이동',   
       slideLabelMessage: '총 {{slidesLength}}장의 배너 중 {{index}}번 배너 입니다.',
     },
     breakpoints: {
@@ -33,23 +38,47 @@ export function swiper() {
         spaceBetween: 30,
       },
     }
+    
   });
-
   $(window).on('resize', function() {
-    swiper.params.spaceBetween = $(window).width() >= 1262 ? 30 : 0;
-    swiper.update(); // Swiper 업데이트
+    mainBannerSwiper.params.spaceBetween = $(window).width() >= 1262 ? 30 : 0;
+    mainBannerSwiper.update(); // Swiper 업데이트
   });
 
   $(cont).mouseenter(function(){
-    swiper.autoplay.stop();
+    mainBannerSwiper.autoplay.stop();
     $(prevBtn).addClass(btnEffect);
     $(nextBtn).addClass(btnEffect);
   })
 
   $(cont).mouseleave(function(){
-    swiper.autoplay.start();
+    mainBannerSwiper.autoplay.start();
     $(prevBtn).removeClass(btnEffect);
     $(nextBtn).removeClass(btnEffect);
   })
 }
 
+function eventBannerSwiperCon() {
+  // 스와이퍼 기본속성
+  var eventBannerSwiper = new Swiper('.event_banner', {
+    slidesPerView : 'auto', 
+    loopAdditionalSlides: 1, 
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+    a11y: { 
+      enabled: true,
+      prevSlideMessage: '이전 이벤트 배너로 이동',
+      nextSlideMessage: '다음 이벤트 배너로 이동',   
+      slideLabelMessage: '총 {{slidesLength}}장의 이벤트 배너 중 {{index}}번 이벤트 배너 입니다.',
+    },
+  });
+}
